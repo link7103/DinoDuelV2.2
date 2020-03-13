7dddfcb3bf27d3a77253034041f7e46c4204c84b
lbonkowski <56656521+lbonkowski@users.noreply.github.com>
2020-03-13 09:20:31 -0600

    explosive class added

---
+++ core/src/com/dinoduel/game/Sprites/Weapons/Explosive.java
@@ -0,0 +1,21 @@
+package com.dinoduel.game.Sprites.Weapons;
+
+import com.badlogic.gdx.physics.box2d.World;
+import com.dinoduel.game.Screens.PlayScreen;
+
+
+
+public abstract class Explosive extends Weapon {
+
+    Explosive(float x, float y, World world, PlayScreen screen) {
+        //adjust how this works - Currently loads one big section, change to loading individuals
+        super(x, y, world, screen);
+    }//end Constructor
+
+    @Override
+    public void useWeapon() {
+
+    }
+
+
+}
--- core/src/com/dinoduel/game/Sprites/Weapons/Grenade.java
+++ core/src/com/dinoduel/game/Sprites/Weapons/Grenade.java
@@ -1,21 +1,36 @@
 package com.dinoduel.game.Sprites.Weapons;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.World;
+import com.dinoduel.game.DinoDuel;
 import com.dinoduel.game.Screens.PlayScreen;
 
+public class Grenade extends Explosive {
 
-
-public class Grenade extends Weapon {
-
-    Grenade(float x, float y, World world, PlayScreen screen) {
-        //adjust how this works - Currently loads one big section, change to loading individuals
+    public Grenade(float x, float y, World world, PlayScreen screen) {
         super(x, y, world, screen);
-    }//end Constructor
+        xSize = 192;
+        ySize = 66;
+        heldXOffset = 0.05f;
+        heldYOffset = -0.03f;
 
-    @Override
-    public void useWeapon() {
 
-    }
 
+
+
+
+
+        damage = 10;
+
+
+        img = new TextureRegion(getTexture(), 198, 54, xSize, ySize);
+
+        defineWeapon();
+
+        setBounds(x, y, xSize / DinoDuel.PPM, ySize / DinoDuel.PPM);
+        setRegion(img);
+        setPosition(wBody.getPosition().x / DinoDuel.PPM - getWidth() / 2, wBody.getPosition().y / DinoDuel.PPM - getHeight() / 2);
+
+    }//end Consrtuctor
 
 }
